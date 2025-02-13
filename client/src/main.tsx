6ed43e9cb20177dcefc3fdeeeec7aa51d94bd9df
RevainIDN <kov.igor94@gmail.com>
2025-02-13 17:35:05 +0200

    Незначительные изменения

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import './translations/i18n';
 import './styles/index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <BrowserRouter basename='/cybersec-app'>
     <App />
-  </StrictMode>,
+  </BrowserRouter>,
 )
