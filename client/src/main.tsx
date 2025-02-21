903d6ec35538b9b78c118cfe70ff8f771dc8feab
RevainIDN <kov.igor94@gmail.com>
2025-02-21 20:42:20 +0200

    Незначительные изменения

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './store/index.ts';
 import './translations/i18n';
 import './styles/index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename='/cybersec-app/'>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter basename='/cybersec-app/'>
+      <App />
+    </BrowserRouter>
+  </Provider>
 )
