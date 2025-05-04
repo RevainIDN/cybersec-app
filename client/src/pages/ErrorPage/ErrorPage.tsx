82e43bb6fe440ecb340e6137abe54415d8b3176f
RevainIDN <kov.igor94@gmail.com>
2025-05-04 20:07:25 +0300

    Добавлена страница ошибки

---
+++ client/src/pages/ErrorPage/ErrorPage.tsx
@@ -0,0 +1,9 @@
+import './ErrorPage.css'
+
+export default function ErrorPage() {
+	return (
+		<div className='not-found section'>
+			<img className='not-found-img' src="404.png" alt="404" />
+		</div>
+	)
+}
